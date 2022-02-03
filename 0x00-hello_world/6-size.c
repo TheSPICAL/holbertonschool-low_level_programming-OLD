71939045566a296998fd4176edaac988836dea46
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-03 09:12:07 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charType;
+	int intType;
+	long longType;
+	long long longlongType;
+	float floatType;
+	
+	// sizeof evaluates the size of a variable
+	printf("Size of char : %zu bytes\n" , sizeof(charType));
+	printf("Size of int : %zu bytes\n" , sizeof(intType));
+	printf("Size of long : %zu bytes\n" , sizeof(longType));
+	printf("Size of long long : %zu bytes\n" , sizeof(longlongType));
+	printf("Size of float : %zu bytes\n" , sizeof(floatType));
+
+	return(0)
+}	
