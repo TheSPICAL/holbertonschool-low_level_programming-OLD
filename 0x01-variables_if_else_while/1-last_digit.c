63138919c59714a96fc336f9469efdb9aba2393b
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-04 03:57:50 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	int lastDigit(int number) { return (number) % 10; }
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n");
+		printf("%d and is greater than 5\n", (n));
 	}
 	else if (n == 0)
 	{
-		printf("%d and is 0\n");
+		printf("%d and is 0\n", (n));
 	}
-	else (n < 6 != 0)
+	else (n < 6 != 0);
 	{
-		printf("%d and is less than 6 and not 0\n");
+		printf("%d and is less than 6 and not 0\n", (n));
 	}
 	return (0);
 }
