9c3c03d61c437181890f436c858e4e9582a43ee2
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-06 05:10:33 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		else
+			putchar(i);
+	}
+	printf("\n");
+
+	return (0);
+}
