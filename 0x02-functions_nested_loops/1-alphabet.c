e001719cb2c33a349a50406b0295de41b162a33d
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-08 04:57:36 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	printf("\n");
+	
+	return (0);
+}
+
