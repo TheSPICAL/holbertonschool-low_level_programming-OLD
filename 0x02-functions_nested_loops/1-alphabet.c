07c035215f6730303e971e1f5a07ec11f1354613
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-09 00:15:05 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - Entry point
+ * print_alphabet - prints the alphabets
  *
- * Return: Always 0 (Success)
- */
-int main()
+ * Return: Always 0.
+*/
+
+void print_alphabet(void)
 {
-	char ch = 'a';
-	printf("Lowercase English Alphabets:\n");
-	while(ch <= 'z')
-{
-	printf("%c ", ch);
-	ch++;
-}
-	printf("\n");
-	return 0;
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		_putchar(A);
+	}
+
+	_putchar('\n');
 }
