ce31572f6628df69e664146341e0541a8bb22025
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-09 04:02:04 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet_x10 - prints the alphabets 10 times
- *
- * Return: Always 0.
 */
 
 void print_alphabet_x10(void)
@@ -11,12 +9,12 @@ void print_alphabet_x10(void)
 	char A;
 	int N;
 
-	for (N = '0'; N <= 9; N++)
+	for (N = 0; N <= 9; N++)
 	{
-	for (A = 'a'; A <= 'z'; A++)
-	{
-		_putchar(A);
+		for (A = 'a'; A <= 'z'; A++)
+		{
+			_putchar(A);
+		}
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
