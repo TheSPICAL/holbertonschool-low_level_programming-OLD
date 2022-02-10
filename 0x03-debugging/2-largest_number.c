5f1a5f7d832a161906235b2123a715a9cae64a7d
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-10 03:31:53 -0800

    fixed input to output the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if ((a >= b && b >= c) || (a >= b && a >= c))
 	{
 		largest = a;
 	}
-	else if (b < a && a > c)
+	else if ((b >= a && a >= c) || (b >= a && b >= c))
 	{
-		largest = c;
+		largest = b;
 	}
 	else
 	{
-		largest = b;
+		largest = c;
 	}
 
 	return (largest);
