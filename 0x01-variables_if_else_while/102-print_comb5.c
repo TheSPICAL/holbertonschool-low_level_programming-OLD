e3762b5a3d95f4473fc94d8d10c550d034a17348
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-15 00:34:27 -0800

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include<stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int n;
+	int m;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (n = '0'; n <= '9'; n++)
+			{
+				for (m = '0'; m <= '9'; m++)
+				{
+					if ((i != n || j != m) && (i * 10 + j < n * 10 + m))
+					{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(n);
+					putchar(m);
+					if (m != '9' || n != '9' || i != '9' || j != '8')
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
