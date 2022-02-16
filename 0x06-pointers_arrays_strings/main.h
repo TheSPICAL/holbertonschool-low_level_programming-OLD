4ad64a7a1466a6f0a2161e93859be18000c7e2f9
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-16 00:18:21 -0800

    main

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include<math.h>
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+#endif
