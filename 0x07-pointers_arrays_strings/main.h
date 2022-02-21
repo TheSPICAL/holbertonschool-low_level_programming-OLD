65c7d393b7d2ea1d82d381743d777fa3938e1667
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-21 01:08:30 -0800

    main

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include<math.h>
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
