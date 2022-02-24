bbf86be6b177ec7c3d1ed84fc778273584b3647c
Youssef Sahbani <youssefmasakni@gmail.com>
2022-02-24 01:51:05 -0800

    main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include<math.h>
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
