50b9bd35ba47eb07d3536415ecfb091382d784a3
Youssef Sahbani <youssefmasakni@gmail.com>
2022-03-07 09:45:29 -0800

    type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - struct that groups the characteristics of the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ * Description: This struct has the data of my_dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	return (dog);
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
+#endif
