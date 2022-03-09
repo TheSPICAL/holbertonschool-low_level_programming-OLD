f3dc07a43129741b3bfe1e65abd506e2261a0550
Youssef Sahbani <youssefmasakni@gmail.com>
2022-03-09 14:49:53 -0800

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: pointer to array
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	f = get_op_func(argv[2]);
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
